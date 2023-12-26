187a8a79190fd06de655a6dfdaf539145dc9ce37
Manjistha Dhar <76396648+man-jis-tha-6240@users.noreply.github.com>
2023-12-26 16:25:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 app.use(express.json())
-
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+  });
 app.use('/api/auth', require('./routes/auth.js'));
 app.use('/api/notes', require('./routes/notes.js'))
 app.listen(port, () => {
